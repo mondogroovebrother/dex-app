8cc202b1e92acbbf807247dbea13b679f0a3c692
mondogroovebrother <mondogroovebrother@gmail.com>
2024-02-06 07:30:03 -0500

    dependencies installed

--- const/details.ts
+++ const/details.ts
@@ -1,6 +1,6 @@
 import { Chain, Mumbai } from "@thirdweb-dev/chains";
 
 export const TOKEN_ADDRESS: string =
-  "0x9b84D0E4fc807CB664aFdBF9E7096bed3a7aB18A";
-export const DEX_ADDRESS: string = "0x92c85c31c74F482AB30D94D7e5c63fe9BD53883B";
+  "0x03a7b306587D81CA24D96c28dA743c6B3C993ba4";
+export const DEX_ADDRESS: string = "0xe6bAA25988A278eE9289C1E54Dff9C0a8B7F088f";
 export const ACTIVE_CHAIN: Chain = Mumbai;
